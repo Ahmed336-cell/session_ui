59a8f9ac96cf8e7b6ecc4183fbe8002448308d53
Ahmed-Maher <ahmaher04@gmail.com>
2024-08-14 17:52:36 +0300

    convert to dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
     );
   }
 }
